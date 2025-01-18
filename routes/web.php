2674558f75168abf19a5b81e7afd75d683375145
adityathok <aditya0thok@gmail.com>
2025-01-18 13:31:32 +0700

    update homepage info

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return 'Your IP Address: ' . $_SERVER['REMOTE_ADDR'] . '<br><small>Copyright © ' . date('Y') . ' Velocity Developer</small>';
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
