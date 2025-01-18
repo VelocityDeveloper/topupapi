79d06682905875fb3356d3f2ecd19555bd25b0e0
adityathok <aditya0thok@gmail.com>
2025-01-18 13:20:04 +0700

    add usercontroller

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProductCategoryController;
 use App\Http\Controllers\ProductBrandController;
@@ -17,6 +18,7 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 Route::middleware(['auth:sanctum'])->group(function () {
 
     Route::apiResources([
+        'users' => UserController::class,
         'products' => ProductController::class,
         'product-category' => ProductCategoryController::class,
         'product-brand' => ProductBrandController::class,
@@ -27,4 +29,5 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/relay', [PriceListController::class, 'index']);
     Route::get('/customer/key/{id}', [CustomerController::class, 'get_key']);
     Route::post('/customer/generatekey/{id}', [CustomerController::class, 'generate_key']);
+    Route::post('/users/password/{id}', [UserController::class, 'update_password']);
 });
