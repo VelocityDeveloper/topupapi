82d6a54441c3116c6905bf22a3bc8a75d53a29ef
adityathok <aditya0thok@gmail.com>
2025-01-18 08:00:01 +0700

    add products seeder dari json

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 use App\Models\Product;
 
 class ProductSeeder extends Seeder
@@ -13,6 +14,14 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        Product::factory()->count(10)->create();
+        // Product::factory()->count(10)->create();
+
+        // Ambil data JSON dari file
+        $json = File::get(database_path('seeders/products.json'));
+        $products = json_decode($json, true);
+
+        foreach ($products as $data) {
+            Product::save_product($data);
+        }
     }
 }
